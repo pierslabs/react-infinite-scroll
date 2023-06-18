ab458bf3a3aef570638cc03bd4a0db061dab74b5
pierslabs <pedrolosasp@gmail.com>
2023-06-18 10:47:49 +0200

    clear logs

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -47,7 +47,6 @@ const Home = () => {
 
       observer.current = new IntersectionObserver((entries) => {
         if (entries[0].isIntersecting && hashMore) {
-          console.log("visible");
           setPage((page) => page + 1);
         }
       }, {});
@@ -56,7 +55,6 @@ const Home = () => {
     },
     [loading, hashMore]
   );
-  console.log({ data: data?.results });
 
   return (
     <Layout>
